f502d6c1caee98459dd1969a75daf85632543f0d
Pasindu Laksara <pasindulaksara@gmail.com>
2022-10-28 01:00:13 +0530

    hi

---
+++ src/context/FeedbackContext.js
@@ -0,0 +1,54 @@
+import { v4 as uuidv4 } from "uuid";
+import { createContext, useState } from "react";
+
+const FeedbackContext = createContext();
+
+export const FeedbackProvider = ({ children }) => {
+  const [feedback, setFeedback] = useState([
+    {
+      id: 1,
+      text: "This item from context",
+      rating: 10,
+    },
+    {
+      id: 2,
+      text: "This item from context 2",
+      rating: 8,
+    },
+    {
+      id: 3,
+      text: "This item from context 3",
+      rating: 8,
+    },
+  ]);
+
+  const [feedbackEdit, setFeedbackEdit] = useState({
+    item: {},
+    edit: false,
+  });
+
+  const deleteFeedback = (id) => {
+    if (window.confirm("Are you sure you want to delete")) {
+      setFeedback(feedback.filter((item) => item.id !== id));
+    }
+  };
+
+  const addFeedback = (newFeedback) => {
+    newFeedback.id = uuidv4();
+    setFeedback([newFeedback, ...feedback]);
+  };
+
+  const editFeedback = (item) => {
+    setFeedbackEdit({
+      item,
+      edit: true,
+    });
+  };
+  return (
+    <FeedbackContext.Provider value={{ feedback, deleteFeedback, addFeedback,editFeedback }}>
+      {children}
+    </FeedbackContext.Provider>
+  );
+};
+
+export default FeedbackContext;
---
+++ src/data/FeedbackData.js
@@ -0,0 +1,18 @@
+const FeedbackData = [
+    
+        {
+            id: 1,
+            rating:10,
+            text: 'lorem Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, facere dolorum magni earum perspiciatis distinctio. Ad beatae reiciendis tenetur null ',
+           },
+    
+           {
+            id: 2,
+            rating:9,
+            text: 'lorem Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, facere dolorum magni earum perspiciatis distinctio. Ad beatae reiciendis tenetur null ',
+           },
+    
+]
+
+export default FeedbackData;
+
